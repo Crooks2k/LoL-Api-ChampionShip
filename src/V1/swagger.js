b046bbe9835aba3bf68a691f93524160eb4060ca
felipe veloza <ciberronaldo@hotmail.com>
2023-02-23 12:34:03 -0500

    swager documentation description

--- src/V1/swagger.js
+++ src/V1/swagger.js
@@ -7,8 +7,18 @@ const path = require("path")
 const options = {
   definition: {
     openapi: "3.0.0",
-    info: {title: "LoL Champion List | API", version: "1.0.0"},
-  },
+    info: {
+      title: "LoL Champion List | API",
+      version: "1.0.0",
+      description: `<hr><h3><strong>Api Rest que permite obtener información varia y detallada sobre los personajes de League Of Legends,
+      cuenta con varias categorias de información en las que se incluye el origen, posicion, nombre, habilidades detalladas e incluso iconos de habilidades de cada personaje.<strong/></h3>\n
+      Podras usar la información que se encuentra alojada en la base de datos de mongo DB para usarla en tu front-end.\n
+        - En esta documentación podras encontrar información acerca de cada uno de los endpoints.\n
+        - Podras realizar peticiones GET, POST, PUT, DELETE con el fin de probar cada conexion.\n
+        - Podras visualizar el esquema de datos usado en la DB [MongoDB] para tenerlo en cuenta a la hora de hacer una insersión de datos POST, PUT, desde el front-end\n<hr>
+      Si deseas mas información acerca del proyecto revisa el repositorio en mi perfil de Github [ @Crooks2k ]\n
+      https://github.com/Crooks2k/LoL-Api-Champions\n`
+  }},
   apis: [`${path.join(__dirname, "../V1/routes/*.js")}`],
   servers: [
     {
